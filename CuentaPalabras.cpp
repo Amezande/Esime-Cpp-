596cdc81d93e1b3a04b4ac774ea0f372ea1bb1d3
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2024-09-02 05:36:19 -0600

    Init cuenta
    Programa en c++ que pretende contar las palabras, caracteres y encuentra la palabra más larga

---
+++ CuentaPalabras.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+//Declaracion de funciones
+int buscarPalabras(string cadena);
+
+int main(){
+    buscarPalabras();
+    return 0;
+}
+
+//funciones definir variables en la funcion
+int buscarPalabras(string cadena){
+    return cadena;
+}
+
+//Lee una cadena con espacios
+/*cout << "Cadena a buscar: ";
+    getline(cin, cadena);
+    cout << "La cadena ingresada es: " << cadena << endl;*/
+
+//Busca en una cadena con espacios
+/*for(int i=0; i<30;i++){
+        switch(cadena[i] ){
+            case ' ': espa++; 
+            break;
+        }
+    }*/
